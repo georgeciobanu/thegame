e469fcfdea872893b39ed55560ab20522e99c46b
George Ciobanu <geo.ciobanu@gmail.com>
2012-02-28 16:54:49 -0700

    Basic Rails app

---
+++ backend/magical/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magical::Application.config.secret_token = 'edeedfb3b5345489997b86c693834dc8d6fedf9494e246748d3adaa366f7f1e4d54fea3f626460f4f343032ec4514d2818e035f7283ecad69344e6a2eaed6580'
