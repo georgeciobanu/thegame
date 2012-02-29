2aa8dde1502f61d1a72288ebc784ab261c54fc65
George Ciobanu <geo.ciobanu@gmail.com>
2012-02-29 14:39:48 -0700

    Migrated to mysql

--- magical/db/schema.rb
+++ magical/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229195818) do
+ActiveRecord::Schema.define(:version => 20120229213835) do
 
   create_table "users", :force => true do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
