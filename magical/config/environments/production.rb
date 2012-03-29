5d99ba36dbc5398acfdd39bfb223792e9dba5bee
George Ciobanu <geo.ciobanu@gmail.com>
2012-03-29 04:01:34 -0700

    Enable even more debugging in prod

--- magical/config/environments/production.rb
+++ magical/config/environments/production.rb
@@ -14,6 +14,17 @@ Magical::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+  
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
