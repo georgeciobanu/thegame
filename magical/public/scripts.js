4bf818931f8ac510f93c9411841ddc7b0726f8bb
Jonathan Cottrell <jonathan@lindenhoney.com>
2012-03-29 08:34:31 -0400

    new landing page

---
+++ magical/public/scripts.js
@@ -0,0 +1,6 @@
+function submitEmail(form) {
+	var email = form.elements['email'].value; 
+	
+	document.getElementById('msg').innerHTML = email + ' successfully added.<br />' 
+	document.getElementById('msg').innerHTML;
+}
